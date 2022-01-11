6ffdd8dc199a8bae5e504c91670412e5e7ff9522
AndreyKo-777 <derrikan84@gmail.com>
2022-01-11 22:58:51 +0200

    1

---
+++ Hometask_2/Hometask_2/Hometask_2/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_2
+{
+    public class Converter
+    {
+        double usd;
+        public double USD { get { return usd; } }
+
+        double eur;
+        public double EUR { get { return EUR; } }
+
+        double rub;
+        public double RUB { get { return RUB; } }
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+                    
+        }
+
+        public void Exchange(string cur, double quantity)
+        {
+            double cash = 0;
+
+            if (cur == "usd") cash = usd * quantity;
+            else if (cur == "eur") cash = eur * quantity;
+            else if (cur == "rub") cash = rub * quantity;
+
+            Console.WriteLine($"Overall is {cash} UAH");
+        }
+
+        public void Exchange(string cur, string cur2, double quantity)
+        {
+            double cash = 0;
+
+            if (cur2 == "usd")
+            {
+                cash = quantity / usd;
+                Console.WriteLine($"Overall is {cash} USD");
+            }
+            else if (cur2 == "eur")
+            {
+                cash = quantity / eur;
+                Console.WriteLine($"Overall is {cash} EUR");
+            }
+
+            else if (cur2 == "rub")
+            {
+                cash = quantity / rub;
+                Console.WriteLine($"Overall is {cash} UAH");
+            }
+
+        }
+
+    }
+}
--- Hometask_2/Hometask_2/Hometask_2/Program.cs
+++ Hometask_2/Hometask_2/Hometask_2/Program.cs
@@ -10,6 +10,29 @@ namespace Hometask_2
     {
         static void Main(string[] args)
         {
+            Converter currency = new Converter(27.53, 31.30, 0.37);
+
+
+            Console.WriteLine("Enter currency you want to change: usd, eur, rub, uah");
+            string cur = Console.ReadLine();
+            if (cur == "uah")
+            { Console.WriteLine("What currency you want to buy? usd, eur, rub");
+                string cur2 = Console.ReadLine();
+                Console.WriteLine("How much UAH you want to spend? ");
+                double quantity = double.Parse(Console.ReadLine());
+
+                currency.Exchange(cur, cur2, quantity);
+
+            }
+            else
+            {
+                Console.WriteLine("Enter quantity");
+                double quantity = double.Parse(Console.ReadLine());
+                currency.Exchange(cur, quantity);
+            }
+        
+
+            Console.ReadKey();
         }
     }
 }
