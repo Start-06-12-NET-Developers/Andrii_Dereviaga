3a3d88dd6dffae5196e2ffd1eb5ee388c2a11eb9
Andriy-Derevyaga <andriy.derevyaga@gmail.com>
2022-01-12 19:25:30 +0200

    InProgress

---
+++ Hometask_2/Hometask_2/Task_2/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Employee
+    {
+        string surname;
+        string name;
+
+        public string Surname
+        {
+            get { return surname; }
+        }
+        public string Name
+        {
+            get { return surname; }
+        }
+
+        public Employee(string surname, string name)
+            {
+            this.surname = surname;
+            this.name = name;
+            }
+
+        public void SalaryCalculator(string position, int workExperience)
+        {
+            double salary = 0;
+            double tax = 0.2;
+        
+        
+        
+        }
+
+    }
+}
---
+++ Hometask_2/Hometask_2/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter surname");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+
+            Employee employee = new Employee(surname, name);
+
+            Console.WriteLine("Enter position");
+            string position = Console.ReadLine();
+
+            Console.WriteLine("Enter work experience");
+            string workExperience = Console.ReadLine();
+
+        }
+    }
+}
